77e5809fa48aee8b4438f52c31ce94a9cd5cf714
Ernesto Fava <e.fava13@gmail.com>
2024-12-11 23:32:25 -0400

    Revert "TailwindCSS"
    This reverts commit d8dfaec678679b3a692cdf615831eb078eec39bc.

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <h1 className="font-bold">Error 404</h1>
+    <div>Error</div>
   )
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,31 @@
-import logo from '../assets/Icon-PC-Amazing.ico'
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
 
 function Home() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <div className="flex flex-row justify-center items-center">
-        <img src={logo} className="size-48" alt="React logo" />
-        <h1 id="brand" className="font-semibold text-Violet-800">PC<span className="ms-3 text-Cyan-600">AMAZING</span></h1>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
